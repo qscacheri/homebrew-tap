9550d2483529241595a5cb920769d6dfd667fefa
Quin Scacheri <qs366@nyu.edu>
2023-06-15 11:57:12 -0400

    fix hash

--- Formula/iterm2warp.rb
+++ Formula/iterm2warp.rb
@@ -2,7 +2,7 @@ class Iterm2warp < Formula
   desc "CLI for converting from iTerm2 themes to Warp themes"
   homepage "https://github.com/qscacheri/iterm2warp"
   url "https://github.com/qscacheri/iterm2warp/releases/download/v0.1.0/iterm2warp-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "96f4f2ac282ca1a948807879c6b31b0f560e33e1"
+  sha256 "ed5060c7f0b7bb92b288be6ca8e9da5461cece7f10db4774f26325dfc1097968"
   version "0.1.0"
 
   def install
